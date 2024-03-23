09a4b1ea948d577a4154c4d9e7e4cadb054b7212
TheAppleFreak <theapplefreak@gmail.com>
2024-03-23 03:51:48 +0000

    Initial commit. Lots of stuff is currently broken, but the dev server will launch at least.

---
+++ v4.webpack.common.js
@@ -0,0 +1,89 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const AssetsPlugin = require("assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src", "ts", "index.ts"),
+    swiper: path.join(__dirname, "src", "ts", "swiper.ts"),
+    map: path.join(__dirname, "src", "ts", "map.ts"),
+    lightbox: path.join(__dirname, "src", "ts", "lightbox.ts")
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[contenthash][ext]"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+
+      {
+        test: /\.((eot)|(woff)|(woff2)|(ttf))(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?name=/[hash].[ext]"
+      },
+
+      {
+        test: /\.(gif|png|jpe?g|svg)/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true
+              }
+            }
+          }
+        ],
+        exclude: /site\/static/
+      },
+
+      {test: /\.json$/, loader: "json-loader"},
+
+      {
+        loader: "babel-loader",
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        query: {cacheDirectory: true}
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        // exclude: /node_modules/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [".ts", ".js", ".css", ".scss"]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      fetch: "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+    }),
+
+    new AssetsPlugin({
+      filename: "webpack.json",
+      path: path.join(process.cwd(), "site/data"),
+      prettyPrint: true
+    }),
+
+    new CopyWebpackPlugin([
+      {
+        from: "./src/fonts/",
+        to: "fonts/",
+        flatten: true
+      }
+    ])
+  ]
+};
